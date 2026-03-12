9e8fefa788f5d52fba93ccdde360c782ba04a6a2
Sarujan Uthayachandran <sarujan_uthayachandran@sluz.ch>
2026-03-12 13:00:55 +0100

    Added NavBar and all Screenviews Closes #31

---
+++ app/src/main/java/com/m306/closetly/explore/ui/ExploreScreen.kt
@@ -0,0 +1,26 @@
+package com.m306.closetly.explore.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ExploreScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Explore", style = MaterialTheme.typography.headlineLarge)
+        Text("Discover outfits & inspiration", style = MaterialTheme.typography.bodyLarge)
+    }
+}
