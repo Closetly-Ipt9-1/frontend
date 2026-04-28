6243a28fcbfe51aa9be15b2b9ea64e10e4850b7d
K4y <116443833+KayTrachsel@users.noreply.github.com>
2026-04-28 22:31:55 +0200

    Reorder Firebase dependencies for consistency and clarity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,15 +69,14 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.firebase:firebase-storage")
     implementation("io.coil-kt:coil-compose:2.7.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("com.google.firebase:firebase-firestore")
 
     implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore:26.1.2")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
 
 }
 
