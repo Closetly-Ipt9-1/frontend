fdd625f5847fd15b5205db9db4b02729a3b5b00c
Saru <168628180+Saru0505@users.noreply.github.com>
2026-04-30 14:14:21 +0200

    Delete app/src/main/java/com/m306/closetly/core/model/User.kt

--- app/src/main/java/com/m306/closetly/core/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.m306.closetly.core.model
-import com.m306.closetly.avatar.AvatarConfig
-
-data class User(
-    val id: String = "",
-    val username: String = "",
-    val avatar: AvatarConfig = AvatarConfig()
-)
